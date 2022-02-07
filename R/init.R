4d1a1724c6137a9654b4b07b631ddff5b29fc8e2
Jakub Nowicki <kuba@appsilon.com>
2022-02-07 14:35:13 +0100

    Split init function.

--- R/init.R
+++ R/init.R
@@ -1,17 +1,30 @@
-#' @importFrom fs dir_create file_create file_delete path
-#' @importFrom usethis use_template proj_set
 #' @export
 init <- function(dir = ".") {
+  init_setup(dir)
+
+  create_app_structure()
+
+  init_cleanup(dir)
+}
+
+#' @importFrom fs dir_create file_create path
+init_setup <- function(dir) {
   dir_create(dir)
   # minimal requirement of usethis to accept directory as R project
   file_create(path(dir, ".here"))
   proj_set(dir)
+}
 
-  use_template(
+#' @importFrom fs file_delete path
+init_cleanup <- function(dir) {
+  file_delete(path(dir, ".here"))
+}
+
+#' @importFrom usethis use_template proj_set
+create_app_structure <- function() {
+    use_template(
     template = "app_structure/app.R",
     save_as = "app.R",
     package = "rhino"
   )
-
-  file_delete(path(dir, ".here"))
 }
