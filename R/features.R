4dea0f1218f3c18c396f940667e7174f720f9f16
Kamil Zyla <kamil@appsilon.com>
2022-02-08 10:56:35 +0100

    feat(rhono): Add a check() function

--- R/features.R
+++ R/features.R
@@ -4,20 +4,27 @@ features <- list(
   ci_github
 )
 
-#' @export
-sync <- function() {
+check_sync <- function(sync) {
   rhino_config <- read_config()
   for (feature in features) {
     config <- rhino_config$features[[feature$key]]
     message(glue::glue("Checking {feature$name}... "), appendLF = FALSE)
     status <- feature$check(config)
     message(status)
-    if (status == "Missing") {
-      message(glue::glue("Adding {feature$name}"))
-      feature$add()
-    } else if (status == "Dangling") {
-      message(glue::glue("Removing {feature$name}"))
-      feature$remove()
+    if (sync) {
+      if (status == "Missing") {
+        message(glue::glue("Adding {feature$name}"))
+        feature$add()
+      } else if (status == "Dangling") {
+        message(glue::glue("Removing {feature$name}"))
+        feature$remove()
+      }
     }
   }
 }
+
+#' @export
+check <- function() check_sync(sync = FALSE)
+
+#' @export
+sync <- function() check_sync(sync = TRUE)
