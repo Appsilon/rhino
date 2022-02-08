025e0e0714bc67e98b23db738f98201fa44474a6
Kamil Zyla <kamil@appsilon.com>
2022-02-08 15:40:38 +0100

    feat(rhino): Logger reexports via box.path setting

--- R/init.R
+++ R/init.R
@@ -1,6 +1,8 @@
 rprofile_init <- function() {
   # This function is called from `.Rprofile` early on and shouldn't depend on any packages.
 
-  # Allow absolute module imports (relative to the app root).
-  options(box.path = file.path(getwd(), "app"))
+  options(box.path = c(
+    file.path(getwd(), "app"), # Allow absolute module imports (relative to the app root).
+    system.file("box_modules", package = "rhino")
+  ))
 }
---
+++ inst/box_modules/rhino/log.R
@@ -0,0 +1,12 @@
+#' @export
+box::use(
+  logger[
+    trace = log_info,
+    debug = log_debug,
+    info = log_info,
+    success = log_success,
+    warn = log_warn,
+    error = log_error,
+    fatal = log_fatal,
+  ]
+)
