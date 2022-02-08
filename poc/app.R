101382f6737014470be335e9e2f9597e99dbb5b8
Kamil Zyla <kamil@appsilon.com>
2022-02-08 16:56:36 +0100

    feat(poc): Use rhino logger config

--- poc/app.R
+++ poc/app.R
@@ -2,24 +2,13 @@
 rm(list = ls(box:::loaded_mods), envir = box:::loaded_mods)
 
 box::use(
-  logger[appender_file, log_appender, log_threshold],
   shiny[addResourcePath, shinyApp],
 )
 box::use(
   r/main,
 )
 
-# nolint start
-LOG_FILE <- Sys.getenv("LOG_FILE")
-LOG_LEVEL <- Sys.getenv("LOG_LEVEL", unset = "INFO")
-# nolint end
-
 addResourcePath("static", "app/static")
-
-log_threshold(LOG_LEVEL)
-if (nzchar(LOG_FILE)) {
-  log_appender(appender_file(LOG_FILE))
-}
 
 shinyApp(
   ui = main$ui("app"),
