dd2bc4d15ed169e473a80a5629f123f6713cfda4
Kamil Zyla <kamil@appsilon.com>
2022-02-08 16:55:37 +0100

    feat(rhino): Configure logger in rprofile_init()

--- R/init.R
+++ R/init.R
@@ -1,8 +1,13 @@
 rprofile_init <- function() {
-  # This function is called from `.Rprofile` early on and shouldn't depend on any packages.
-
   options(box.path = c(
-    file.path(getwd(), "app"), # Allow absolute module imports (relative to the app root).
-    system.file("box_modules", package = "rhino")
+    fs::path_wd("app"), # Allow absolute module imports (relative to the app root).
+    fs::path_package("rhino", "box_modules")
   ))
+
+  log_level <- Sys.getenv("RHINO_LOG_LEVEL", unset = "INFO")
+  log_file <- Sys.getenv("RHINO_LOG_FILE", unset = NA)
+  logger::log_threshold(log_level)
+  if (!is.na(log_file)) {
+    logger::log_appender(logger::appender_file(log_file))
+  }
 }
