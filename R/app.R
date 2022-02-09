fa75b8210bd7805fb4bb6142c7628b52daee3137
Kamil Zyla <kamil@appsilon.com>
2022-02-09 10:21:26 +0100

    feat(rhino): Include stylesheet in app() if Sass is enabled

--- R/app.R
+++ R/app.R
@@ -1,10 +1,18 @@
 app <- function() {
+  sass_enabled <- "sass" %in% names(read_config()$features)
+
   # Purge the box module cache, so the app can be reloaded without restarting the R session.
   rm(list = ls(box:::loaded_mods), envir = box:::loaded_mods)
   box::use(r/main)
+
   shiny::addResourcePath("static", "app/static")
   shiny::shinyApp(
-    ui = main$ui("app"),
+    ui = shiny::tagList(
+      if (sass_enabled) shiny::tags$head(
+        shiny::tags$link(rel = "stylesheet", type = "text/css", href = "static/css/app.min.css"),
+      ),
+      main$ui("app")
+    ),
     server = function(input, output) {
       main$server("app")
     }
