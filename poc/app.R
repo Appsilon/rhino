868ffe9f50f857f43b456c15b1d65fb438bafad0
Kamil Zyla <kamil@appsilon.com>
2022-02-09 10:09:27 +0100

    feat(poc): Replace app.R with rhino:::app()

--- poc/app.R
+++ poc/app.R
@@ -1,18 +1,1 @@
-# Purge the box module cache, so the app can be reloaded without restarting the R session.
-rm(list = ls(box:::loaded_mods), envir = box:::loaded_mods)
-
-box::use(
-  shiny[addResourcePath, shinyApp],
-)
-box::use(
-  r/main,
-)
-
-addResourcePath("static", "app/static")
-
-shinyApp(
-  ui = main$ui("app"),
-  server = function(input, output) {
-    main$server("app")
-  }
-)
+rhino:::app()
