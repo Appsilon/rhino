cb99b947fe535eb278ca94f1b8695f9d1fd14fc0
Kamil Zyla <kamil@appsilon.com>
2022-02-10 13:48:09 +0100

    feat(rhino): Install Node.js files in .rhino/node

--- R/node.R
+++ R/node.R
@@ -1,3 +1,17 @@
 run_yarn <- function(...) {
   system2("yarn", c("--cwd", "node", ...))
 }
+
+install_node <- function() {
+  node_path <- fs::path(".rhino", "node")
+  if (fs::dir_exists(node_path)) return()
+  fs::dir_copy(
+    path = fs::path_package("rhino", "node"),
+    new_path = node_path,
+    overwrite = TRUE
+  )
+  fs::link_create(
+    path = fs::path("..", ".."),
+    new_path = fs::path(node_path, "root")
+  )
+}
