ee9872d8dac0abf1ac9f62bf7a6bdcbac6c41bef
Kamil Zyla <kamil@appsilon.com>
2022-02-17 10:02:55 +0100

    feat: Expose objects exported from index.js in App global

--- inst/templates/node/webpack.config.babel.js
+++ inst/templates/node/webpack.config.babel.js
@@ -4,6 +4,7 @@ export default {
   mode: 'production',
   entry: join(__dirname, 'root', 'app', 'js', 'index.js'),
   output: {
+    library: 'App',
     path: join(__dirname, 'root', 'app', 'static', 'js'),
     filename: 'app.min.js',
   },
