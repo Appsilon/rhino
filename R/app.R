e3ffa9ef70135eff9256b0fcfa4f47dc7524213a
Kamil Zyla <kamil@appsilon.com>
2022-02-24 09:42:27 +0100

    refactor: Improve rhino:::app() and avoid R CMD check warnings

--- R/app.R
+++ R/app.R
@@ -1,18 +1,30 @@
-app <- function() {
-  # Purge the box module cache, so the app can be reloaded without restarting the R session.
-  rm(list = ls(box:::loaded_mods), envir = box:::loaded_mods)
-
-  log_file <- Sys.getenv("LOG_FILE")
+configure_logger <- function() {
   log_level <- Sys.getenv("LOG_LEVEL", unset = "INFO")
+  log_file <- Sys.getenv("LOG_FILE", unset = NA)
 
   logger::log_threshold(log_level)
-  if (nzchar(log_file)) {
+  if (!is.na(log_file)) {
     logger::log_appender(logger::appender_file(log_file))
   }
+}
 
-  shiny::addResourcePath("static", "app/static")
+load_main_module <- function() {
+  # Purge the box module cache, so the app can be reloaded without restarting the R session.
+  loaded_mods <- loadNamespace("box")$loaded_mods
+  rm(list = ls(loaded_mods), envir = loaded_mods)
+
+  # Silence "no visible binding" notes on R CMD check.
+  r <- NULL
+  main <- NULL
 
   box::use(r/main)
+  main
+}
+
+app <- function() {
+  configure_logger()
+  shiny::addResourcePath("static", "app/static")
+  main <- load_main_module()
   shiny::shinyApp(
     ui = main$ui("app"),
     server = function(input, output) {
