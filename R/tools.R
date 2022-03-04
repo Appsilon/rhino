27170e614dcda51c81e0655093b2c6d3795f2881
Kamil Zyla <kamil@appsilon.com>
2022-03-04 09:14:19 +0100

    feat: Add watch and fix arguments for Node tools

--- R/tools.R
+++ R/tools.R
@@ -64,9 +64,11 @@ format_r <- function(path) {
 #'   tags$button("Hello!", onclick = "App.sayHello()")
 #' ```
 #'
+#' @param watch Keep the process running and rebuilding JS whenever source files change.
+#'
 #' @export
-build_js <- function() {
-  yarn("build-js")
+build_js <- function(watch = FALSE) {
+  yarn("build-js", if (watch) "--watch")
 }
 
 # nolint start
@@ -89,20 +91,28 @@ build_js <- function() {
 #' See the [ESLint documentation](https://eslint.org/docs/user-guide/configuring/rules#using-configuration-comments-1)
 #' for full details.
 #'
+#' @param fix Automatically fix problems.
+#'
 #' @export
 # nolint end
-lint_js <- function() {
-  yarn("lint-js")
+lint_js <- function(fix = FALSE) {
+  yarn("lint-js", if (fix) "--fix")
 }
 
 #' Build Sass
 #'
+#' @param watch Keep the process running and rebuilding Sass whenever source files change.
+#' Only supported for `sass: node` config in `rhino.yml`.
+#'
 #' @export
-build_sass <- function() {
+build_sass <- function(watch = TRUE) {
   config <- read_config()$sass
   if (config == "node") {
-    yarn("build-sass")
+    yarn("build-sass", if (watch) "--watch")
   } else if (config == "r") {
+    if (watch) {
+      cli::cli_alert_warning("The {.arg watch} argument is only supported when using Node.")
+    }
     output_dir <- fs::path("app", "static", "css")
     fs::dir_create(output_dir)
     sass::sass(
@@ -115,9 +125,11 @@ build_sass <- function() {
 
 #' Lint Sass
 #'
+#' @param fix Automatically fix problems.
+#'
 #' @export
-lint_sass <- function() {
-  yarn("lint-sass")
+lint_sass <- function(fix = FALSE) {
+  yarn("lint-sass", if (fix) "--fix")
 }
 
 #' Run Cypress end-to-end tests
