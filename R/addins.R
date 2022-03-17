a2e8d04bf711679eb99bd956f1efa9446e48733c
Jakub Nowicki <kuba@appsilon.com>
2022-03-17 15:18:32 +0100

    feat: Add addin for creating Rhino module.

---
+++ R/addins.R
@@ -0,0 +1,21 @@
+create_module <- function() {
+  rstudioapi::insertText(
+"box::use(
+  shiny[moduleServer, NS]
+)
+
+#' @export
+ui <- function(id) {
+  ns <- NS(id)
+
+}
+
+#' @export
+server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+
+  })
+}
+"
+  )
+}
