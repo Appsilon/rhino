9f7eef67f214d4830ba29435f1cca7a982c78879
Jakub Nowicki <kuba@appsilon.com>
2022-03-31 12:52:36 +0200

    feat: Add rhino dataset.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Population of rhinos
+#'
+#' A dataset containing population of 5 species of rhinos
+#'
+#' @format A data frame with 58 rows and 3 variables:
+#' \describe{
+#'   \item{Year}{year}
+#'   \item{Population}{rhinos population}
+#'   \item{Species}{rhinos species}
+#' }
+#' @source \url{https://ourworldindata.org/}
+"rhinos"
