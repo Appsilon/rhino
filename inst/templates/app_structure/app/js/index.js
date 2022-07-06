7d6e203d3afd926e28e34d37a44312e23bc8b4ae
Pavel Demin <paveldemin@me.com>
2022-07-06 19:19:33 +0800

    feat: add boilerplate to js template

---
+++ inst/templates/app_structure/app/js/index.js
@@ -0,0 +1,16 @@
+/*
+ * If the following line is highlighted as error,
+ * you should install yarn dependencies.
+ * To do this, simply run `rhino::lint_js()` once.
+ */
+
+/// <reference types="../../.rhino/node/node_modules/@types/rstudio-shiny/" />
+
+/*
+ * This is a speciall comment that enables
+ * TypeScript checks in JavaScript files.
+ * It allows you to maximize the utility of
+ * including Shiny type definitions.
+ */
+
+// @ts-check
