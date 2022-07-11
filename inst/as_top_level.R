f288465359696884ddf13f26f990de2ff9a7f26b
Kamil Zyla <kamil@appsilon.com>
2022-07-11 12:04:26 +0200

    docs: Explain why as_top_level is defined in inst

--- inst/as_top_level.R
+++ inst/as_top_level.R
@@ -1,3 +1,5 @@
+# This function is defined in the `inst` directory, as it must be sourced with `keep.source = TRUE`
+# for reloading to work: https://github.com/Appsilon/rhino/issues/157
 function(shiny_module) {
   list(
     # Wrap the UI in a function to support Shiny bookmarking.
