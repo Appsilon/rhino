955f386680c8128bdb03029dca6b55b6f71062f3
Jakub Nowicki <kuba@appsilon.com>
2023-01-31 14:24:09 +0100

    chore: Simplify the init app.

--- inst/templates/app_structure/app/main.R
+++ inst/templates/app_structure/app/main.R
@@ -1,24 +1,25 @@
 box::use(
-  shiny[bootstrapPage, div, moduleServer, NS, renderText, tags, textOutput],
+  shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
   bootstrapPage(
-    div(
-      style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
-      tags$h1(
-        textOutput(ns("message")),
-        tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
-      )
-    )
+    uiOutput(ns("message"))
   )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$message <- renderText("Hello!")
+    output$message <- renderUI({
+      div(
+        style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
+        tags$h1(
+          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
+        )
+      )
+    })
   })
 }
