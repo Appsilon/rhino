e6532f738fa13136f697a909a55c7031dc71a368
Recle E. Vibal <recle.vibal@appsilon.com>
2023-05-17 15:47:30 +0800

    Feat: Confirms lint errors and fixes
    Adds scripts to create scripts in R, js, and scss
    with bad styling and confirms that there are
    lint errors thrown and fixing these lints return
    the properly styled files.

--- tests/e2e/test-lint-r.R
+++ tests/e2e/test-lint-r.R
@@ -1,13 +1,7 @@
-rhino::format_r(paths = c("app", "tests"))
-
+rhino::lint_r()
 # Create bad scripts and test if formatting returns the expected result
 test_file_path <- fs::path("app", "logic", "bad-style.R")
 cat("bad_object_style=12", file = test_file_path)
-rhino::format_r(paths = "app")
-testthat::expect_equal(
-  readLines(test_file_path),
-  "bad_object_style <- 12"
-)
-
+testthat::expect_error(rhino::lint_r())
 # Clean up
 file.remove(test_file_path)
