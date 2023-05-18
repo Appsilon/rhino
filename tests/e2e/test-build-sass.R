ae55d59198b8e031c14ca549815829e02063c73f
Recle E. Vibal <recle.vibal@appsilon.com>
2023-05-18 14:26:45 +0800

    Feat: Adds test for build js and sass

--- tests/e2e/test-build-sass.R
+++ tests/e2e/test-build-sass.R
@@ -1,2 +1,29 @@
 rhino::build_sass()
-stopifnot(file.exists(fs::path("app", "static", "css", "app.min.css")))
+
+min_css_path <- fs::path("app", "static", "css", "app.min.css")
+main_css_path <- fs::path("app", "styles", "main.scss")
+
+testthat::expect_equal(
+  readLines(min_css_path), ""
+)
+
+# Create minimal css and check app.min.css output
+cat(
+  ".myClass { color: #fff; }\n",
+  file = main_css_path,
+  append = TRUE
+)
+rhino::build_sass()
+testthat::expect_equal(
+  readLines(min_css_path),
+  ".myClass{color:#fff}"
+)
+# Clean up
+cat(
+  "",
+  file = main_css_path
+)
+cat(
+  "",
+  file = min_css_path
+)
