43c5c986b37a2f4688a057c6c098150159eb3e6c
Recle E. Vibal <recle.vibal@appsilon.com>
2023-05-31 14:25:19 +0800

    Feat: Tests rhino main functions
    Removes singles tests for build js and sass.
    Output of build sass and build js should be confirmed
    in the cypress tests.

--- tests/e2e/app-files/hello.spec.js
+++ tests/e2e/app-files/hello.spec.js
@@ -30,4 +30,26 @@ describe('Say Hello', () => {
     cy.get('#app-hello-name').clear();
     cy.get('#app-hello-say_hello').should('be.disabled');
   });
+
+  it('should style elements', () => {
+    const inputName = 'Rhino';
+    cy.get('#app-hello-name').type(inputName);
+    cy.get('#app-hello-say_hello').click();
+
+    cy.get('.input-and-click')
+      .should('have.css', 'display', 'inline-flex');
+
+    cy.get('#app-hello-say_hello')
+      .should('have.css', 'color', 'rgb(255, 255, 255)')
+      // check if border: none
+      .and('have.css', 'border-width', '0px')
+      .and('have.css', 'border-style', 'none')
+      .and('have.css', 'border-color', 'rgb(255, 255, 255)')
+      .and('have.css', 'background-color', 'rgb(0, 153, 249)');
+
+    cy.get('#app-hello-message')
+      .should('have.css', 'display', 'flex')
+      .and('have.css', 'align-items', 'center')
+      .and('have.css', 'justify-content', 'center');
+  });
 })
--- tests/e2e/test-build-js.R
+++ tests/e2e/test-build-js.R
@@ -7,10 +7,7 @@ cat(
   file = index_js_path
 )
 rhino::build_js()
-testthat::expect_identical(
-  readLines(min_js_path),
-  readLines(fs::path("..", "test-files", "sayHello-app-min-js.txt"))
-)
+testthat::expect_true(readLines(min_js_path) != "var App;App={};")
 
 # Revert to empty script and check otuput
 cat(
