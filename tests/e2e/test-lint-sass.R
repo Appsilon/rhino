b59692dd7f42b7c95e41582f636018d90fad8b29
Kamil Zyla <kamil@appsilon.com>
2024-01-04 17:52:50 +0100

    fix: Adjust stylelint rules and fix E2E tests

--- tests/e2e/test-lint-sass.R
+++ tests/e2e/test-lint-sass.R
@@ -2,12 +2,12 @@ rhino::lint_sass()
 
 # Create bad scripts and test if formatting returns the expected result
 test_scss_path <- fs::path("app", "styles", "bad-style.scss")
-cat(".myClass{color: #FFFFFF}", file = test_scss_path)
+cat(".my-class{color: #FFFFFF}", file = test_scss_path)
 testthat::expect_error(rhino::lint_sass())
 rhino::lint_sass(fix = TRUE)
 testthat::expect_identical(
   readLines(test_scss_path),
-  ".myClass { color: #fff; }"
+  ".my-class { color: #fff; }"
 )
 # Clean up
 file.remove(test_scss_path)
