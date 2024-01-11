caced920eacd3d23bda57c01e7bf10977d6282d6
Rodrigo Basa <rodrigo@appsilon.com>
2024-01-11 17:03:30 +0800

    box function import count linter

---
+++ tests/testthat/test-linter_box_function_import_count_linter.R
@@ -0,0 +1,67 @@
+five_function_imports <- "box::use(
+  dplyr[
+    arrange,
+    select,
+    mutate,
+    distinct,
+    filter,
+  ],
+)"
+
+five_function_imports_inline <- "box::use(
+  dplyr[arrange, select, mutate, distinct, filter, ],
+)"
+
+six_function_imports <- "box::use(
+  dplyr[
+    arrange,
+    select,
+    mutate,
+    distinct,
+    filter,
+    slice,
+  ],
+)"
+
+six_function_imports_inline <- "box::use(
+  dplyr[arrange, select, mutate, distinct, filter, slice, ],
+)"
+
+three_function_imports <- "box::use(
+  dplyr[
+    arrange,
+    select,
+    mutate,
+  ],
+)"
+
+three_function_imports_inline <- "box::use(
+  dplyr[arrange, select, mutate, ],
+)"
+
+lint_message <- function(max = 5L) {
+  rex::rex(glue::glue("Limit the function imports to a max of {max}."))
+}
+
+test_that("box_func_import_count_linter skips allowed function count.", {
+  linter <- box_func_import_count_linter()
+  
+  lintr::expect_lint(five_function_imports, NULL, )
+  lintr::expect_lint(five_function_imports_inline, NULL, linter)
+})
+
+test_that("box_func_import_count_linter skips allowed function count supplied max", {
+  linter <- box_func_import_count_linter(max = 3)
+  
+  lintr::expect_lint(three_function_imports, NULL, )
+  lintr::expect_lint(three_function_imports_inline, NULL, linter)
+})
+
+test_that("box_func_import_count_linter blocks function imports more than max", {
+  max <- 5
+  linter <- box_func_import_count_linter(max = max)
+  lint_msg <- lint_message(max)
+  
+  lintr::expect_lint(six_function_imports, list(message = lint_msg), linter)
+  lintr::expect_lint(six_function_imports_inline, list(message = lint_msg), linter)
+})
