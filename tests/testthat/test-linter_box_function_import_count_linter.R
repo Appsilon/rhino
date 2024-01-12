4523b4d2c59a0e5c6bee78b6bde7b31d07fe74d1
Rodrigo Basa <rodrigo@appsilon.com>
2024-01-12 17:23:07 +0800

    somehow forgot to call the linter

--- tests/testthat/test-linter_box_function_import_count_linter.R
+++ tests/testthat/test-linter_box_function_import_count_linter.R
@@ -59,14 +59,14 @@ lint_message <- function(max = 5L) {
 test_that("box_func_import_count_linter skips allowed function count.", {
   linter <- box_func_import_count_linter(max = 5)
 
-  lintr::expect_lint(five_function_imports, NULL, )
+  lintr::expect_lint(five_function_imports, NULL, linter)
   lintr::expect_lint(five_function_imports_inline, NULL, linter)
 })
 
 test_that("box_func_import_count_linter skips allowed function count supplied max", {
   linter <- box_func_import_count_linter(max = 3)
 
-  lintr::expect_lint(three_function_imports, NULL, )
+  lintr::expect_lint(three_function_imports, NULL, linter)
   lintr::expect_lint(three_function_imports_inline, NULL, linter)
 })
 
