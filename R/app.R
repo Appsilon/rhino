4d62b95a75fc3298a0859254548043498d4e320c
Kamil Zyla <kamil@appsilon.com>
2024-02-07 15:46:58 +0100

    refactor: Reorganize app.R - move app() to top

--- R/app.R
+++ R/app.R
@@ -1,3 +1,73 @@
+#' Rhino application
+#'
+#' The entrypoint for a Rhino application.
+#' Your `app.R` should contain nothing but a call to `rhino::app()`.
+#'
+#' This function is a wrapper around `shiny::shinyApp()`.
+#' It reads `rhino.yml` and performs some configuration steps (logger, static files, box modules).
+#' You can run a Rhino application in typical fashion using `shiny::runApp()`.
+#'
+#' Rhino will load the `app/main.R` file as a box module (`box::use(app/main)`).
+#' It should export two functions which take a single `id` argument -
+#' the `ui` and `server` of your top-level Shiny module.
+#'
+#' # Legacy entrypoint
+#'
+#' It is possible to specify a different way to load your application
+#' using the `legacy_entrypoint` option in `rhino.yml`:
+#' 1. `app_dir`: Rhino will run the app using `shiny::shinyAppDir("app")`.
+#' 2. `source`: Rhino will `source("app/main.R")`.
+#' This file should define the top-level `ui` and `server` objects to be passed to `shinyApp()`.
+#' 3. `box_top_level`: Rhino will load `app/main.R` as a box module (as it does by default),
+#' but the exported `ui` and `server` objects will be considered as top-level.
+#'
+#' The `legacy_entrypoint` setting is useful when migrating an existing Shiny application to Rhino.
+#' It is recommended to transform your application step by step:
+#' 1. With `app_dir` you should be able to run your application right away
+#' (just put the files in the `app` directory).
+#' 2. With `source` setting your application structure must be brought closer to Rhino,
+#' but you can still use `library()` and `source()` functions.
+#' 3. With `box_top_level` you can be confident that the whole app is properly modularized,
+#' as box modules can only load other box modules (`library()` and `source()` won't work).
+#' 4. The last step is to remove the `legacy_entrypoint` setting completely.
+#' Compared to `box_top_level` you'll need to make your top-level `ui` and `server`
+#' into a [Shiny module](https://shiny.rstudio.com/articles/modules.html)
+#' (functions taking a single `id` argument).
+#'
+#' @return An object representing the app (can be passed to `shiny::runApp()`).
+#'
+#' @examples
+#' \dontrun{
+#'   # Your `app.R` should contain nothing but this single call:
+#'   rhino::app()
+#' }
+#' @export
+app <- function() {
+  setup_box_path()
+  box::purge_cache()
+  configure_logger()
+  shiny::addResourcePath("static", fs::path_wd("app", "static"))
+
+  entrypoint <- read_config()$legacy_entrypoint
+  if (identical(entrypoint, "app_dir")) {
+    return(shiny::shinyAppDir("app"))
+  }
+
+  if (identical(entrypoint, "source")) {
+    main <- new.env()
+    source(fs::path("app", "main.R"), local = main)
+  } else {
+    main <- load_main_module()
+    if (!identical(entrypoint, "box_top_level")) {
+      main <- as_top_level(main)
+    }
+  }
+  shiny::shinyApp(
+    ui = with_head_tags(main$ui),
+    server = main$server
+  )
+}
+
 setup_box_path <- function() {
   # Normally `box.path` is set in `.Rprofile` and used for the whole R session,
   # however `shinytest2` launches the application in a new process which doesn't source `.Rprofile`.
@@ -72,74 +142,4 @@ with_head_tags <- function(ui) {
   } else {
     wrap(ui)
   }
-}
-
-#' Rhino application
-#'
-#' The entrypoint for a Rhino application.
-#' Your `app.R` should contain nothing but a call to `rhino::app()`.
-#'
-#' This function is a wrapper around `shiny::shinyApp()`.
-#' It reads `rhino.yml` and performs some configuration steps (logger, static files, box modules).
-#' You can run a Rhino application in typical fashion using `shiny::runApp()`.
-#'
-#' Rhino will load the `app/main.R` file as a box module (`box::use(app/main)`).
-#' It should export two functions which take a single `id` argument -
-#' the `ui` and `server` of your top-level Shiny module.
-#'
-#' # Legacy entrypoint
-#'
-#' It is possible to specify a different way to load your application
-#' using the `legacy_entrypoint` option in `rhino.yml`:
-#' 1. `app_dir`: Rhino will run the app using `shiny::shinyAppDir("app")`.
-#' 2. `source`: Rhino will `source("app/main.R")`.
-#' This file should define the top-level `ui` and `server` objects to be passed to `shinyApp()`.
-#' 3. `box_top_level`: Rhino will load `app/main.R` as a box module (as it does by default),
-#' but the exported `ui` and `server` objects will be considered as top-level.
-#'
-#' The `legacy_entrypoint` setting is useful when migrating an existing Shiny application to Rhino.
-#' It is recommended to transform your application step by step:
-#' 1. With `app_dir` you should be able to run your application right away
-#' (just put the files in the `app` directory).
-#' 2. With `source` setting your application structure must be brought closer to Rhino,
-#' but you can still use `library()` and `source()` functions.
-#' 3. With `box_top_level` you can be confident that the whole app is properly modularized,
-#' as box modules can only load other box modules (`library()` and `source()` won't work).
-#' 4. The last step is to remove the `legacy_entrypoint` setting completely.
-#' Compared to `box_top_level` you'll need to make your top-level `ui` and `server`
-#' into a [Shiny module](https://shiny.rstudio.com/articles/modules.html)
-#' (functions taking a single `id` argument).
-#'
-#' @return An object representing the app (can be passed to `shiny::runApp()`).
-#'
-#' @examples
-#' \dontrun{
-#'   # Your `app.R` should contain nothing but this single call:
-#'   rhino::app()
-#' }
-#' @export
-app <- function() {
-  setup_box_path()
-  box::purge_cache()
-  configure_logger()
-  shiny::addResourcePath("static", fs::path_wd("app", "static"))
-
-  entrypoint <- read_config()$legacy_entrypoint
-  if (identical(entrypoint, "app_dir")) {
-    return(shiny::shinyAppDir("app"))
-  }
-
-  if (identical(entrypoint, "source")) {
-    main <- new.env()
-    source(fs::path("app", "main.R"), local = main)
-  } else {
-    main <- load_main_module()
-    if (!identical(entrypoint, "box_top_level")) {
-      main <- as_top_level(main)
-    }
-  }
-  shiny::shinyApp(
-    ui = with_head_tags(main$ui),
-    server = main$server
-  )
 }
