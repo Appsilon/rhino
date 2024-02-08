28dc62075d0721aa9472faf35cca8b8e778d2a6f
Kamil Zyla <kamil@appsilon.com>
2024-02-08 11:34:24 +0100

    refactor: Use describe() and it() in app.R tests

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,35 +1,39 @@
-test_that("configure_logger() works with missing config fields", {
-  mockery::stub(configure_logger, "config::get", list())
-  expect_message(configure_logger())
+describe("configure_logger()", {
+  it("works with missing config fields", {
+    mockery::stub(configure_logger, "config::get", list())
+    expect_message(configure_logger())
 
-  mockery::stub(configure_logger, "config::get", list(rhino_log_level = "INFO"))
-  expect_message(configure_logger())
+    mockery::stub(configure_logger, "config::get", list(rhino_log_level = "INFO"))
+    expect_message(configure_logger())
 
-  mockery::stub(configure_logger, "config::get", list(rhino_log_file = "my.log"))
-  expect_message(configure_logger())
+    mockery::stub(configure_logger, "config::get", list(rhino_log_file = "my.log"))
+    expect_message(configure_logger())
 
-  mockery::stub(
-    configure_logger,
-    "config::get",
-    list(rhino_log_level = "INFO", rhino_log_file = "my.log")
-  )
-  expect_silent(configure_logger())
+    mockery::stub(
+      configure_logger,
+      "config::get",
+      list(rhino_log_level = "INFO", rhino_log_file = "my.log")
+    )
+    expect_silent(configure_logger())
+  })
 })
 
-test_that("normalize_ui() handles UI defined as a tag", {
-  ui <- shiny::tags$div("test")
-  wrapped <- normalize_ui(ui)
-  expect_identical(wrapped("request"), ui)
-})
+describe("normalize_ui()", {
+  it("handles UI defined as a tag", {
+    ui <- shiny::tags$div("test")
+    wrapped <- normalize_ui(ui)
+    expect_identical(wrapped("request"), ui)
+  })
 
-test_that("normalize_ui() handles UI defined as a function without parameters", {
-  ui <- function() shiny::tags$div("test")
-  wrapped <- normalize_ui(ui)
-  expect_identical(wrapped("request"), ui())
-})
+  it("handles UI defined as a function without parameters", {
+    ui <- function() shiny::tags$div("test")
+    wrapped <- normalize_ui(ui)
+    expect_identical(wrapped("request"), ui())
+  })
 
-test_that("normalize_ui() handles UI defined as a function with a request parameter", {
-  ui <- function(request) shiny::tags$div(request)
-  wrapped <- normalize_ui(ui)
-  expect_identical(wrapped("request"), ui("request"))
+  it("handles UI defined as a function with a request parameter", {
+    ui <- function(request) shiny::tags$div(request)
+    wrapped <- normalize_ui(ui)
+    expect_identical(wrapped("request"), ui("request"))
+  })
 })
