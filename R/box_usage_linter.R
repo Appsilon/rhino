d68840e0bf18dfb21973f413b6849e8bf187f06f
Rodrigo Basa <rodrigo@appsilon.com>
2024-03-06 17:08:54 +0800

    linter can handle package$function

--- R/box_usage_linter.R
+++ R/box_usage_linter.R
@@ -11,39 +11,69 @@ box_usage_linter <- function() {
     expr/SYMBOL and
     OP-LEFT-BRACKET
   ]
-  /expr/SYMBOL[
-    preceding::OP-LEFT-BRACKET and
-    following::OP-RIGHT-BRACKET
+  /expr[
+    preceding-sibling::OP-LEFT-BRACKET and
+    following-sibling::OP-RIGHT-BRACKET
   ]
+  /SYMBOL
   "
   
+  box_package_import <- "
+  /child::expr[
+    SYMBOL
+  ]
+  "
+
   xpath_package_functions <- paste(box_base_path, box_package_functions)
+  xpath_package_import <- paste(box_base_path, box_package_import)
   
-  xpath_function_calls <- "
-  //SYMBOL_FUNCTION_CALL[
+  xpath_box_function_calls <- "
+  //expr[
+    SYMBOL_FUNCTION_CALL[
+      not(text() = 'use')
+    ] and
     not(
-      preceding-sibling::SYMBOL_PACKAGE[text() = 'box'] and
-      text() = 'use'
+      SYMBOL_PACKAGE[text() = 'box']
     )
   ] |
   //SPECIAL
   "
-  
+
   lintr::Linter(function(source_expression) {
     if (!lintr::is_lint_level(source_expression, "file")) {
       return(list())
     }
     
     xml <- source_expression$full_xml_parsed_content
-    
+
     # get list of imported package functions
     imported_functions <- xml2::xml_find_all(xml, xpath_package_functions)
     imported_functions_text <- xml2::xml_text(imported_functions)
     # remove back-ticks "`%>%`" -> "%>%"
     imported_functions_text <- gsub("`", "", imported_functions_text)
     
+    imported_packages <- xml2::xml_find_all(xml, xpath_package_import)
+    imported_packages_text <- xml2::xml_text(imported_packages)
+    imported_package_functions <- lapply(imported_packages_text, function(pkg) {
+        tryCatch(
+          getNamespaceExports(pkg),
+          error = function(e) character()
+        )
+      })
+    names(imported_package_functions) <- imported_packages_text
+    
+    imported_package_function_list <- unlist(
+      lapply(names(imported_package_functions), function(pkg) {
+        paste(
+          pkg,
+          imported_package_functions[[pkg]],
+          sep = "$"
+        )
+      })
+    )
+    
     # get list of function calls
-    function_calls <- xml2::xml_find_all(xml, xpath_function_calls)
+    function_calls <- xml2::xml_find_all(xml, xpath_box_function_calls)
     function_calls_text <- xml2::xml_text(function_calls)
     
     # remove base function calls
@@ -54,13 +84,24 @@ box_usage_linter <- function() {
     unimported_functions <- lapply(function_calls, function(fun_call) {
       fun_call_text <- xml2::xml_text(fun_call)
       
-      if (!fun_call_text %in% imported_functions_text) {
-        lintr::xml_nodes_to_lints(
-          fun_call,
-          source_expression = source_expression,
-          lint_message = "Function not imported.",
-          type = "warning"
-        )
+      if (grepl(".+\\$.+", fun_call_text)) {
+        if (!fun_call_text %in% imported_package_function_list) {
+          lintr::xml_nodes_to_lints(
+            fun_call,
+            source_expression = source_expression,
+            lint_message = "package$function does not exist.",
+            type = "warning"
+          )
+        }
+      } else {
+        if (!fun_call_text %in% imported_functions_text) {
+          lintr::xml_nodes_to_lints(
+            fun_call,
+            source_expression = source_expression,
+            lint_message = "Function not imported.",
+            type = "warning"
+          )
+        }
       }
     })
     
--- tests/testthat/test-linter_box_usage_linter.R
+++ tests/testthat/test-linter_box_usage_linter.R
@@ -9,14 +9,28 @@ test_that("box_usage_linter skips allowed box-imported package functions", {
     select(mpg, cyl) %>%
     filter(mpg <= 10)
   "
+  
+  good_box_call_2 <- "box::use(
+    shiny[ns],
+    glue,
+    fs[path_file],
+  )
+  
+  name <- 'Fred'
+  glue$glue('My name is {name}.')
+  
+  path_file('dir/file.zip')
+  "
 
   lintr::expect_lint(good_box_call_1, NULL, linter)
 })
 
 test_that("box_usage_linter blocks functions not box-imported", {
   linter <- box_usage_linter()
-  lint_message <- "Function not imported."
+  lint_message_1 <- rex::rex("Function not imported.")
+  lint_message_2 <- rex::rex("package$function does not exist.")
   
+  # filter not imported
   bad_box_call_1 <- "box::use(
     dplyr[`%>%`, select],
   )
@@ -26,13 +40,27 @@ test_that("box_usage_linter blocks functions not box-imported", {
     filter(mpg <= 10)
   "
   
-  lintr::expect_lint(bad_box_call_1, list(message = lint_message), linter)
+  # xyz is not exported by glue
+  bad_box_call_2 <- "box::use(
+    glue,
+    fs[path_file],
+  )
+  
+  name <- 'Fred'
+  glue$xyz('My name is {name}.')
+  
+  path_file('dir/file.zip')
+  "
+  
+  lintr::expect_lint(bad_box_call_1, list(message = lint_message_1), linter)
+  lintr::expect_lint(bad_box_call_2, list(message = lint_message_2), linter)
 })
 
 test_that("box_usage_linter blocks box-imported functions unused", {
   linter <- box_usage_linter()
-  lint_message <- "Imported function unused."
+  lint_message <- rex::rex("Imported function unused.")
   
+  # filter is unused
   bad_box_call_1 <- "box::use(
     dplyr[`%>%`, select, filter],
   )
