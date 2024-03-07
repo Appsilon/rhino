3f52d8cf0723fa9b875bf4c3b2f300af0dd89336
Rodrigo Basa <rodrigo@appsilon.com>
2024-03-07 15:32:46 +0800

    don't lint on base packages

--- R/box_usage_linter.R
+++ R/box_usage_linter.R
@@ -5,7 +5,7 @@ box_pkg_fun_exists_linter <- function() {
   /parent::expr
   /parent::expr
   "
-  
+
   box_package_functions <- "
   /child::expr[
     expr/SYMBOL and
@@ -20,18 +20,18 @@ box_pkg_fun_exists_linter <- function() {
     )
   ]
   "
-  
+
   xpath_package_functions <- paste(box_base_path, box_package_functions)
-  
+
   lintr::Linter(function(source_expression) {
     if (!lintr::is_lint_level(source_expression, "file")) {
       return(list())
     }
-    
+
     xml <- source_expression$full_xml_parsed_content
-    
+
     pkg_fun_not_exists <- check_attached_pkg_funs(xml, xpath_package_functions)
-    
+
     lapply(pkg_fun_not_exists$xml, function(xml_node) {
       lintr::xml_nodes_to_lints(
         xml_node,
@@ -41,7 +41,6 @@ box_pkg_fun_exists_linter <- function() {
       )
     })
   })
-  
 }
 
 #' @export
@@ -51,7 +50,7 @@ box_usage_linter <- function() {
   /parent::expr
   /parent::expr
   "
-  
+
   box_package_functions <- "
   /child::expr[
     expr/SYMBOL and
@@ -66,13 +65,13 @@ box_usage_linter <- function() {
     )
   ]
   "
-  
+
   box_package_import <- "
   /child::expr[
     SYMBOL
   ]
   "
-  
+
   box_package_three_dots <- "
   /child::expr[
     expr/SYMBOL[text() = '...']
@@ -82,11 +81,11 @@ box_usage_linter <- function() {
   ]
   /SYMBOL
   "
-  
+
   xpath_package_functions <- paste(box_base_path, box_package_functions)
   xpath_package_import <- paste(box_base_path, box_package_import)
   xpath_package_three_dots <- paste(box_base_path, box_package_three_dots)
-  
+
   xpath_box_function_calls <- "
   //expr[
     SYMBOL_FUNCTION_CALL[
@@ -98,55 +97,53 @@ box_usage_linter <- function() {
   ] |
   //SPECIAL
   "
-  
+
   lintr::Linter(function(source_expression) {
     if (!lintr::is_lint_level(source_expression, "file")) {
       return(list())
     }
-    
+
     xml <- source_expression$full_xml_parsed_content
-    
+
     attached_functions <- get_attached_functions(xml, xpath_package_functions)
     attached_three_dots <- get_attached_three_dots(xml, xpath_package_three_dots)
-    
     all_attached_fun <- c(attached_functions$text, attached_three_dots$text)
-    
+
     attached_packages <- get_attached_packages(xml, xpath_package_import)
-    
-    function_calls <- xml2::xml_find_all(xml, xpath_box_function_calls)
-    function_calls_text <- xml2::xml_text(function_calls)
-    
-    # remove base function calls
-    
-    unimported_functions <- lapply(function_calls, function(fun_call) {
+    function_calls <- get_function_calls(xml, xpath_box_function_calls)
+    base_pkgs <- get_base_packages()
+
+    unimported_functions <- lapply(function_calls$xml_nodes, function(fun_call) {
       fun_call_text <- xml2::xml_text(fun_call)
-      
-      if (grepl(".+\\$.+", fun_call_text)) {
-        if (!fun_call_text %in% attached_packages$text) {
-          lintr::xml_nodes_to_lints(
-            fun_call,
-            source_expression = source_expression,
-            lint_message = "package$function does not exist.",
-            type = "warning"
-          )
-        }
-      } else {
-        if (!fun_call_text %in% all_attached_fun) {
-          lintr::xml_nodes_to_lints(
-            fun_call,
-            source_expression = source_expression,
-            lint_message = "Function not imported.",
-            type = "warning"
-          )
+
+      if (!fun_call_text %in% base_pkgs$text) {
+        if (grepl(".+\\$.+", fun_call_text)) {
+          if (!fun_call_text %in% attached_packages$text) {
+            lintr::xml_nodes_to_lints(
+              fun_call,
+              source_expression = source_expression,
+              lint_message = "package$function does not exist.",
+              type = "warning"
+            )
+          }
+        } else {
+          if (!fun_call_text %in% all_attached_fun) {
+            lintr::xml_nodes_to_lints(
+              fun_call,
+              source_expression = source_expression,
+              lint_message = "Function not imported.",
+              type = "warning"
+            )
+          }
         }
       }
     })
-    
+
     unused_function_imports <- lapply(attached_functions$xml, function(fun_import) {
       fun_import_text <- xml2::xml_text(fun_import)
       fun_import_text <- gsub("`", "", fun_import_text)
-      
-      if (!fun_import_text %in% function_calls_text) {
+
+      if (!fun_import_text %in% function_calls$text) {
         lintr::xml_nodes_to_lints(
           fun_import,
           source_expression = source_expression,
@@ -155,7 +152,7 @@ box_usage_linter <- function() {
         )
       }
     })
-    
+
     c(
       unimported_functions,
       unused_function_imports
@@ -165,12 +162,12 @@ box_usage_linter <- function() {
 
 check_attached_pkg_funs <- function(xml, xpath) {
   pkg_imports <- xml2::xml_find_all(xml, xpath)
-  
+
   xpath_pkg_names <- "
     expr/SYMBOL[
       parent::expr/following-sibling::OP-LEFT-BRACKET
     ]"
-  
+
   xpath_just_functions <- "
       expr[
         preceding-sibling::OP-LEFT-BRACKET and
@@ -182,17 +179,17 @@ check_attached_pkg_funs <- function(xml, xpath) {
         )
       ]
     "
-  
+
   not_exported <- lapply(pkg_imports, function(pkg_import) {
     xml2::xml_find_all(pkg_import, xpath_just_functions)
-    
+
     packages <- extract_xml_and_text(pkg_import, xpath_pkg_names)
     exported_functions <- unlist(get_packages_exports(packages$text))
     attached_functions <- extract_xml_and_text(pkg_import, xpath_just_functions)
-    
+
     attached_functions$xml[!attached_functions$text %in% exported_functions]
   })
-  
+
   list(
     xml = not_exported
   )
@@ -205,9 +202,9 @@ get_packages_exports <- function(pkg_list) {
       error = function(e) character()
     )
   })
-  
+
   names(exported_funs) <- pkg_list
-  
+
   exported_funs
 }
 
@@ -215,7 +212,7 @@ extract_xml_and_text <- function(xml, xpath) {
   xml_nodes <- xml2::xml_find_all(xml, xpath)
   text <- lintr::get_r_string(xml_nodes)
   text <- gsub("`", "", text)
-  
+
   list(
     xml_nodes = xml_nodes,
     text = text
@@ -236,7 +233,7 @@ get_attached_functions <- function(xml, xpath) {
 "
   xpath_attached_functions <- paste(xpath, xpath_just_functions)
   attached_functions <- extract_xml_and_text(xml, xpath_attached_functions)
-  
+
   list(
     xml = attached_functions$xml_nodes,
     text = attached_functions$text
@@ -245,10 +242,10 @@ get_attached_functions <- function(xml, xpath) {
 
 get_attached_three_dots <- function(xml, xpath) {
   attached_three_dots <- extract_xml_and_text(xml, xpath)
-  
+
   nested_list <- get_packages_exports(attached_three_dots$text)
   flat_list <- unlist(nested_list, use.names = FALSE)
-  
+
   list(
     xml = attached_three_dots$xml_nodes,
     nested = nested_list,
@@ -258,9 +255,9 @@ get_attached_three_dots <- function(xml, xpath) {
 
 get_attached_packages <- function(xml, xpath) {
   attached_packages <- extract_xml_and_text(xml, xpath)
-  
+
   nested_list <- get_packages_exports(attached_packages$text)
-  
+
   flat_list <- unlist(
     lapply(names(nested_list), function(pkg) {
       paste(
@@ -270,10 +267,32 @@ get_attached_packages <- function(xml, xpath) {
       )
     })
   )
-  
+
   list(
     xml = nested_list$xml_nodes,
     nested = nested_list,
     text = flat_list
+  )
+}
+
+get_base_packages <- function() {
+  base_pkgs_names <- sessionInfo()$basePkgs
+  base_pkgs_funs <- get_packages_exports(base_pkgs_names)
+  base_pkgs_funs_flat <- unlist(base_pkgs_funs, use.names = FALSE)
+
+  list(
+    nested = base_pkgs_funs,
+    text = base_pkgs_funs_flat
+  )
+}
+
+get_function_calls <- function(xml, xpath) {
+  # lintr::get_r_string throws an error when seeing SYMBOL %>%
+  xml_nodes <- xml2::xml_find_all(xml, xpath)
+  text <- xml2::xml_text(xml_nodes, trim = TRUE)
+
+  list(
+    xml_nodes = xml_nodes,
+    text = text
   )
 }
--- tests/testthat/test-linter_box_usage_linter.R
+++ tests/testthat/test-linter_box_usage_linter.R
@@ -1,7 +1,7 @@
 test_that("box_usage_linter skips allowed box-imported package functions", {
   linter <- box_usage_linter()
-  
-  good_box_call_1 <- "box::use(
+
+  good_box_usage_1 <- "box::use(
     dplyr[`%>%`, select, filter],
   )
 
@@ -9,86 +9,101 @@ test_that("box_usage_linter skips allowed box-imported package functions", {
     select(mpg, cyl) %>%
     filter(mpg <= 10)
   "
-  
-  good_box_call_2 <- "box::use(
+
+  good_box_usage_2 <- "box::use(
     shiny[NS],
     glue,
     fs[path_file],
   )
-  
+
   name <- 'Fred'
   glue$glue('My name is {name}.')
-  
+
   path_file('dir/file.zip')
-  
+
   ns <- NS()
   "
-  
-  good_box_call_3 <- "box::use(
+
+  good_box_usage_3 <- "box::use(
     glue[...]
   )
-  
+
   name <- 'Fred'
   glue_sql('My name is {name}.')
   "
 
-  lintr::expect_lint(good_box_call_1, NULL, linter)
-  lintr::expect_lint(good_box_call_2, NULL, linter)
-  lintr::expect_lint(good_box_call_3, NULL, linter)
+  good_box_usage_4 <- "box::use(
+    dplyr[`%>%`, filter, pull],
+  )
+
+  mpg <- mtcars %>%
+    filter(mpg <= 10) %>%
+    pull(mpg)
+
+  mean(mpg)
+  "
+
+  lintr::expect_lint(good_box_usage_1, NULL, linter)
+  lintr::expect_lint(good_box_usage_2, NULL, linter)
+  lintr::expect_lint(good_box_usage_3, NULL, linter)
+  lintr::expect_lint(good_box_usage_4, NULL, linter)
 })
 
 test_that("box_usage_linter blocks functions not box-imported", {
   linter <- box_usage_linter()
   lint_message_1 <- rex::rex("Function not imported.")
   lint_message_2 <- rex::rex("package$function does not exist.")
-  
+
   # filter not imported
-  bad_box_call_1 <- "box::use(
+  bad_box_usage_1 <- "box::use(
     dplyr[`%>%`, select],
   )
-  
+
   mtcars %>%
     select(mpg, cyl) %>%
-    filter(mpg <= 10)
+    mutate(
+      m = mean(mpg)
+    )
   "
-  
+
   # xyz is not exported by glue
-  bad_box_call_2 <- "box::use(
+  bad_box_usage_2 <- "box::use(
     glue,
     fs[path_file],
   )
-  
+
   name <- 'Fred'
   glue$xyz('My name is {name}.')
   
   path_file('dir/file.zip')
   "
-  
-  bad_box_call_3 <- "box::use(
+
+  # xyz function does not exist
+  bad_box_usage_3 <- "box::use(
     glue[...]
   )
-  
+
   name <- 'Fred'
   xyz('My name is {name}')
   "
-  
-  lintr::expect_lint(bad_box_call_1, list(message = lint_message_1), linter)
-  lintr::expect_lint(bad_box_call_2, list(message = lint_message_2), linter)
-  lintr::expect_lint(bad_box_call_3, list(message = lint_message_1), linter)
+
+  lintr::expect_lint(bad_box_usage_1, list(message = lint_message_1), linter)
+  lintr::expect_lint(bad_box_usage_2, list(message = lint_message_2), linter)
+  lintr::expect_lint(bad_box_usage_3, list(message = lint_message_1), linter)
 })
 
 test_that("box_usage_linter blocks box-imported functions unused", {
   linter <- box_usage_linter()
   lint_message <- rex::rex("Imported function unused.")
-  
+
   # filter is unused
-  bad_box_call_1 <- "box::use(
+  bad_box_usage_1 <- "box::use(
     dplyr[`%>%`, select, filter],
   )
-  
+
   mtcars %>%
     select(mpg, cyl)
   "
-  
-  lintr::expect_lint(bad_box_call_1, list(message = lint_message), linter)
+
+  lintr::expect_lint(bad_box_usage_1, list(message = lint_message), linter)
 })
